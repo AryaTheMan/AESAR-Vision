2c88d565272985a5b2645c6c42c69a6c1d46d094
Aryaman Bhattacharjee <bhattacharjeearyaman2@gmail.com>
2026-09-14 11:45:49 +0530

    Configure GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/AESAR-Vision/",
   plugins: [react()],
-  server: { proxy: { "/api": "http://127.0.0.1:8000" } },
+  server: {
+    proxy: {
+      "/api": "http://127.0.0.1:8000",
+    },
+  },
 });
